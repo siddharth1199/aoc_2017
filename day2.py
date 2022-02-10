5cdf7ef1578b413ebd6133ff44e39d34916d86c1
Sheshadri <ssheshad@LAMU02F837MMD6R.uhc.com>
2022-02-10 13:13:54 +0000

    Day 2

--- day2.py
+++ day2.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
 
-WS = pd.read_csv('day2.txt', delim_whitespace=True, header=None)
-np_arr = np.array(WS)
-print('Part 1 =', (np.max(np_arr, axis=1) - np.min(np_arr, axis=1)).sum())
+grid = pd.read_csv('day2.txt', delim_whitespace=True, header=None)
+np_grid = np.array(grid)
+print('Part 1 =', (np.max(np_grid, axis=1) - np.min(np_grid, axis=1)).sum())
